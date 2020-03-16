a2171b79f18cc88e11b4c9c6fc9098ccab7a530f
TechnologyAnimal <TechnologyAnimal@gmail.com>
2020-03-15 17:02:32 -0700

    Add temporary workaround until concurrent writes to same csv file is implemented.

--- Example/Invoke-TlsProtocolTest.ps1
+++ Example/Invoke-TlsProtocolTest.ps1
@@ -1,32 +1,50 @@
+PARAM (
+    $InputCsvFilePath = 'input.csv',
+    $OutputCsvFilePath = 'output.csv'
+)
 # This is an example of using powershell 7's ForEach-Object -Parallel to spawn off concurrent threads to scan multiple systems simultaneously.
 function Invoke-TlsProtocolTest {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
-        [string]$InputCsvFilePath,
-        [int32]$ThrottleLimit
+        [Parameter(Mandatory)][string]$InputCsvFilePath,
+        [Parameter(Mandatory)][string]$OutputCsvFilePath,
+        [Switch]$Force
     )
     
     begin {
         if (-not (Test-Path $InputCsvFilePath)) {
             Write-Error "Cannot find $InputCsvFilePath`." -ErrorAction Stop
         }
-        if(-not $ThrottleLimit) {
-            if ($isWindows) {
-                Write-Verbose "isWindows: $isWindows"
-                [int32]$ThrottleLimit = (Get-WmiObject Win32_Processor).Count
+
+        if (Test-Path $OutputCsvFilePath) {
+            if ($Force) {
+                Remove-Item -Path $OutputCsvFilePath
             }
-            elseif ($isMacOS) {
+            else {
+                Write-Error "OutputCsvFilePath $OutputCsvFilePath already exists. Use -Force to overwrite." -ErrorAction Stop
+            }
+        }
+
+        if(-not $ThrottleLimit) {
+            if ($isMacOS) {
                 [int32]$ThrottleLimit = sysctl -n hw.logicalcpu
+                $slash = '/'
                 Write-Verbose "isMacOS: $isMacOS`."
             }
             elseif ($isLinux) {
                 [int32]$ThrottleLimit = python -c 'import multiprocessing as mp; print(mp.cpu_count())'
+                $slash = '/'
                 Write-Verbose "isLinux: $isLinux`."
             }
             else {
-                $ThrottleLimit = 5 # Microsoft's default value for ForEach-Object -Parallel
-                Write-Verbose "Cannot determine OS."
+                try {
+                    [int32]$ThrottleLimit = (Get-WmiObject Win32_Processor).Count
+                }
+                catch {
+                    $ThrottleLimit = 5 # Microsoft's default value for ForEach-Object -Parallel
+                }
+                $slash = '\'
+                Write-Verbose "isWindows: $true"
             }
         }
         Write-Verbose "InputCsvFilePath: $InputCsvFilePath"
@@ -35,6 +53,8 @@ function Invoke-TlsProtocolTest {
     
     process {
         $ConcurrentDictionary = [System.Collections.Concurrent.ConcurrentDictionary[string,object]]::new()
+        $Guid = (New-Guid).Guid
+        New-Item $Guid -ItemType Directory
         # TO-DO: Add threadsafe ooperation to save output to a single csv file
         # $ConcurrentQueue = [System.Collections.Concurrent.ConcurrentQueue[object]]
         Import-Csv -Path $InputCsvFilePath | ForEach-Object -Parallel {
@@ -44,18 +64,25 @@ function Invoke-TlsProtocolTest {
             $dictitonary = $using:ConcurrentDictionary
             Write-Verbose "Fqdn: $fqdn"
             Write-Verbose "Ports: $ports"
-            Import-Module "../Test-TlsProtocols/Test-TlsProtocols.psm1"
-            $result = Test-TlsProtocols -Server $server -Port $Port -IncludeRemoteCertificateInfo
-            $dictitonary.TryAdd($server,$result)
+            Import-Module "Test-TlsProtocols"
+            $tempDir = $using:Guid
+            $osSlash = $using:slash
+            $hashtable = Test-TlsProtocols -Server $server -Port $Port -IncludeRemoteCertificateInfo
+            $tempFile = $tempDir + $osSlash + $(New-Guid).Guid + '.csv'
+            Write-Verbose $tempFile
+            $dictitonary.TryAdd($server,$hashtable)
+            $psobject = [PSCustomObject]$hashtable
+            Export-Csv $tempFile -InputObject $psobject -Encoding utf8
         } -ThrottleLimit $ThrottleLimit
+        Get-ChildItem $Guid -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv $OutputCsvFilePath -NoTypeInformation -Append
+        Remove-Item $Guid -Recurse
     }
     end {
-        $ConcurrentDictionary
+        Import-Csv $OutputCsvFilePath | ForEach-Object {
+            # Do more work based upon results
+            $_
+        }
     }
 } # Invoke-TlsProtocolTest
-if (-not (Get-Module 'Test-TlsProtocols')) {
-    Import-Module 'Test-TlsProtocols'
-}
-$InputCsvFilePath = 'example.csv'
-$ThrottleLimit = 8
-Invoke-TlsProtocolTest -InputCsvFilePath $InputCsvFilePath -ThrottleLimit $ThrottleLimit
+
+Invoke-TlsProtocolTest -InputCsvFilePath $InputCsvFilePath -OutputCsvFilePath $OutputCsvFilePath -Force
